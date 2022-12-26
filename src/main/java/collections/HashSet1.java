0c6369a53ef7cc9d8d35601beb0542884e9a23bd
sanjaysanju6 <89413396+sanjaysanju6@users.noreply.github.com>
2022-12-26 15:00:57 -0500

    Hashset Example

---
+++ src/main/java/collections/HashSet1.java
@@ -0,0 +1,20 @@
+package collections;
+
+import java.util.*;
+class HashSet1 {
+    public static void main(String args[]){
+        //Creating HashSet and adding elements
+        HashSet<String> set = new HashSet();
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+        set.add("Four");
+        set.add("Five");
+        set.remove("Five");
+        Iterator<String> i=set.iterator();
+        while(i.hasNext())
+        {
+            System.out.println(i.next());
+        }
+    }
+}
