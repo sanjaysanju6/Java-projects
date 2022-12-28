1fb34a93f61127a803ef4b8d2d4505a352163c51
sanjaysanju6 <89413396+sanjaysanju6@users.noreply.github.com>
2022-12-28 11:13:20 -0500

    Updated ThreadLamda Runnable demo

--- src/main/java/Thread/LamdaThread.java
+++ src/main/java/Thread/LamdaThread.java
@@ -3,29 +3,26 @@ package Thread;
 public class LamdaThread {
     public static void main(String[] args)
     {
-        Runnable obj = ()->
-        {
-            for (int i = 0; i <= 5; i++) {
-                System.out.println("Hi");
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
+            Thread t1 = new Thread(()->
+            {
+                for (int i = 0; i <= 5; i++) {
+                    System.out.println("Hi");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                    }
                 }
-            }
-            };
-        Runnable obj1 = ()->
-        {
-            for (int i = 0; i <= 5; i++) {
-                System.out.println("Hello");
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
+            });
+            Thread t2 = new Thread(()->
+            {
+                for (int i = 0; i <= 5; i++) {
+                    System.out.println("Hello");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                    }
                 }
-            }
-        };
-
-            Thread t1 = new Thread(obj);
-            Thread t2 = new Thread(obj1);
+            });
 
             t1.start();
             try {
