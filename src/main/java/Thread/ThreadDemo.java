56521e1711fdfd836deab234fe3172c247f4efb7
sanjaysanju6 <89413396+sanjaysanju6@users.noreply.github.com>
2022-12-28 10:59:22 -0500

    Thread Runnable demo

--- src/main/java/Thread/ThreadDemo.java
+++ src/main/java/Thread/ThreadDemo.java
@@ -1,9 +1,30 @@
 package Thread;
-
+class Hi implements Runnable{
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
+class Hello implements Runnable {
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
 public class ThreadDemo {
     public static void main(String[] args) {
-        Hi obj = new Hi();
-        Hello obj1 = new Hello();
+        Runnable obj = new Hi();
+        Runnable obj1 = new Hello();
 
         Thread t1 = new Thread(obj);
         Thread t2 = new Thread(obj1);
